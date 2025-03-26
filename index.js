7cca52586db7f462ebc633ca511c7a36bdd28cc5
Alexander <alexsander@proxsis.net>
2025-03-26 09:04:22 -0300

    fear: endpoint to get uf by id

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/ufs', (req, res) => {
     res.json(collectionUfs.colecaoUf);
 });
 
+app.get('/ufs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const uf = collectionUfs.colecaoUf.find(uf => uf.id === id);
+    res.json(uf);
+});
+
 app.listen(8080, () => {
     const data = new Date();
     console.log(`Server started at ${data}`);
